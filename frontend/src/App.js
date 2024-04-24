76d993c03d606c7808db9cc0d92dbb0ff6cbe076
Trung Tran <trungtran63470@gmail.com>
2024-04-23 22:59:55 -0700

    initialized homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import RegistrationForm from './pages/Register';
 import LoginForm from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element=<div>hi welcome to BeSocial</div> />
+      <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
